99b4acf18073df1d0f21efc4b01f0f016a660ff7
mirta <stefanac.mirta@gmail.com>
2024-06-21 12:56:39 +0200

    +added controllers, routes and env file

--- index.js
+++ index.js
@@ -1,56 +1,34 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const Product = require('./models/product.model');
+const express = require("express");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
+const dotenv = require('dotenv');
+const app = express();
 
-const res = require('express/lib/response');
-const app = express()
-
-
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+dotenv.config(); //enable .env variables
+
+//routes
+app.use("/api/products/", productRoute)
+
 
 app.get('/', function (req, res) {
-  res.send('Hello from Node API!');
+  console.log("Hello home!")yield
 });
 
-app.get('/api/products', async (req, res) => {
-  try{
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-      res.status(500).json({message: error.message});
-  }
-});
 
-app.get
-
-app.get('/api/products/:id', async (req, res) => {
-  try{
-    const products = await Product.find
-    res.status(200).json(products);
-  } catch (error) {
-      res.status(500).json({message: error.message});
-  }
-});
-
-app.post('/api/products', async (req, res) => {
-  try{
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-      res.status(500).json({message: error.message});
-  }
-});
 
 
 //connection string
-mongoose.connect('mongodb+srv://admin:admin@testdb.9awek7w.mongodb.net/Node-API?retryWrites=true&w=majority&appName=TestDB')
+mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST)
   .then(() => {
     console.log('Connected to the database!')
     app.listen(3000), () => {
         console.log("Server is running on port 3000")
     }
-})
-  .catch(() => console.log("Connection failed!"));
+}).catch(() => console.log("Connection failed!"));
 
 
 
